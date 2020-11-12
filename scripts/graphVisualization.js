ff31597588868740e4e022db5e7cf32b306ad700
Akarsh-B <akarshbalu@gmail.com>
2020-11-12 12:02:12 -0600

    Added Screen divisions

--- scripts/graphVisualization.js
+++ scripts/graphVisualization.js
@@ -1,58 +1,57 @@
-var width = 960,
-    height = 500
+var width = 100,
+    height = 200
 
-d3.json("../data/graphFile.json").then(function(data) {
+d3.json("../data/graphFile.json").then(function (data) {
     console.log(data);
 
-const links = data.links.map(d => Object.create(d));
-const nodes = data.nodes.map(d => Object.create(d));
+    const links = data.links.map(d => Object.create(d));
+    const nodes = data.nodes.map(d => Object.create(d));
 
-console.log(nodes)
+    console.log(nodes)
 
-const svg = d3.select('.graph-vis').append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .style("background-color", "pink")
+    const svg = d3.select('.graph-vis').append("svg")
+        .attr("width", width)
+        .attr("height", height)
 
-  const simulation = d3.forceSimulation(nodes)
-      .force("link", d3.forceLink(links).id(d => d.id))
-      .force("charge", d3.forceManyBody())
-      .force("center", d3.forceCenter(width / 2, height / 2));
+    const simulation = d3.forceSimulation(nodes)
+        .force("link", d3.forceLink(links).id(d => d.id))
+        .force("charge", d3.forceManyBody())
+        .force("center", d3.forceCenter(width / 2, height / 2));
 
-  const link = svg.append("g")
-      .attr("stroke", "#999")
-      .attr("stroke-opacity", 0.6)
-    .selectAll("line")
-    .data(links)
-    .join("line")
-      .attr("stroke-width", d => Math.sqrt(d.value));
+    const link = svg.append("g")
+        .attr("stroke", "#999")
+        .attr("stroke-opacity", 0.6)
+        .selectAll("line")
+        .data(links)
+        .join("line")
+        .attr("stroke-width", d => Math.sqrt(d.value));
 
-  const node = svg.append("g")
-      .attr("stroke", "#fff")
-      .attr("stroke-width", 1.5)
-    .selectAll("circle")
-    .data(nodes)
-    .join("circle")
-      .attr("r", 5)
-      .attr("fill", red)
-      .call(drag(simulation));
+    const node = svg.append("g")
+        .attr("stroke", "#fff")
+        .attr("stroke-width", 1.5)
+        .selectAll("circle")
+        .data(nodes)
+        .join("circle")
+        .attr("r", 5)
+        .attr("fill", red)
+        .call(drag(simulation));
 
-  node.append("title")
-      .text(d => d.id);
+    node.append("title")
+        .text(d => d.id);
 
-  simulation.on("tick", () => {
-    link
-        .attr("x1", d => d.source.x)
-        .attr("y1", d => d.source.y)
-        .attr("x2", d => d.target.x)
-        .attr("y2", d => d.target.y);
+    simulation.on("tick", () => {
+        link
+            .attr("x1", d => d.source.x)
+            .attr("y1", d => d.source.y)
+            .attr("x2", d => d.target.x)
+            .attr("y2", d => d.target.y);
 
-    node
-        .attr("cx", d => d.x)
-        .attr("cy", d => d.y);
-  });
+        node
+            .attr("cx", d => d.x)
+            .attr("cy", d => d.y);
+    });
 
-  invalidation.then(() =>
-   simulation.stop());
+    invalidation.then(() =>
+        simulation.stop());
 
 });
